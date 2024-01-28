53c49539ca48f6a34fb48fd2c361ae38cc61ce55
Boby Singh <126677943+Boby-Singh@users.noreply.github.com>
2024-01-28 12:15:22 +0530

    Update vite.config.js

--- Krashi_Bazar/vite.config.js
+++ Krashi_Bazar/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import React from 'react';
+import ViteReactPlugin from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-
-    plugins: [react()],
-})
+export default {
+  plugins: [ViteReactPlugin()],
+};
